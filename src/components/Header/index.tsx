b17445c814397348466e431851970f21db9fab38
Bruno De Masi <bruno.demasi@terras.agr.br>
2021-05-24 14:59:27 -0300

    feat(Home): showing prismic posts data

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.container}>
-      <img src="/logo.svg" alt="logo" />
+      <Link href="/">
+        <img src="/logo.svg" alt="logo" />
+      </Link>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -38,48 +42,37 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
       <Header />
 
       <main className={`${styles.container} ${commonStyles.container}`}>
-        <a className={styles.post}>
-          <article>
-            <h1>Como utilizar hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida</p>
+        {postsPagination.results.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a className={styles.post}>
+              <article>
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
 
-            <div className={styles.postInfo}>
-              <time>
-                <FiCalendar /> 15 Mar 2021
-              </time>
+                <div className={styles.postInfo}>
+                  <time>
+                    <FiCalendar /> {post.first_publication_date}
+                  </time>
 
-              <span>
-                <FiUser />
-                Bruno De Masi
-              </span>
-            </div>
-          </article>
-        </a>
-        <a className={styles.post}>
-          <article>
-            <h1>Como utilizar hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida</p>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
+              </article>
+            </a>
+          </Link>
+        ))}
 
-            <div className={styles.postInfo}>
-              <time>
-                <FiCalendar /> 15 Mar 2021
-              </time>
-
-              <span>
-                <FiUser />
-                Bruno De Masi
-              </span>
-            </div>
-          </article>
-        </a>
-
-        <button
-          type="button"
-          className={styles.loadMorePostsButton}
-          onClick={() => console.log()}
-        >
-          Carregar mais posts
-        </button>
+        {postsPagination.next_page && (
+          <button
+            type="button"
+            className={styles.loadMorePostsButton}
+            onClick={() => console.log()}
+          >
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
@@ -87,9 +80,37 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  // const postsResponse = await prismic.query();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      pageSize: 5,
+      page: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => ({
+    uid: post.uid,
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+  }));
 
   return {
-    props: {},
+    props: {
+      postsPagination: {
+        results: posts,
+        next_page: postsResponse.next_page,
+      },
+    },
+    revalidate: 60 * 30, // 30 minutes
   };
 };
